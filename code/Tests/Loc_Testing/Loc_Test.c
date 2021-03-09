95c3c3478651f69f92c82d8385b934a4ceff1ba6
Jason Braker <jbraker@mail.bradley.edu>
2021-03-09 16:26:05 -0600

    Updated code

--- code/Tests/Loc_Testing/Loc_Test.c
+++ code/Tests/Loc_Testing/Loc_Test.c
@@ -1,4 +1,4 @@
-/* Multiplexer Array Test 
+/* 
 *    
 */
 
@@ -15,6 +15,7 @@
 #include <rc/uart.h>
 #include <rc/gpio.h>
 #include <rc/start_stop.h>
+#include <rc/motor.h>
 
 // Custom headers
 #define DEBUG_XBEECOM 0
@@ -27,6 +28,10 @@
 #define STEPS_PER_MEASUREMENT 5
 #define DEG_PER_MEASUREMENT (int)(STEPS_PER_MEASUREMENT*DEG_PER_STEP)
 #define NUM_MEASUREMENTS 90/DEG_PER_MEASUREMENT
+#define MOVING_AVG_SIZE 10 // Size of the moving average window
+
+#define MOTOR_LEFT 1
+#define MOTOR_RIGHT 2
 
 // interrupt handler to catch ctrl-c
 static void __signal_handler(__attribute__ ((unused)) int dummy)
@@ -36,9 +41,21 @@ static void __signal_handler(__attribute__ ((unused)) int dummy)
 }
 
 // Local functions
-int getMeasurement(struct XBeeArray_Settings array, unsigned int current, unsigned char* strengths, unsigned char* distance_strengths, unsigned char* directional_strengths);
+int getMeasurement(struct XBeeArray_Settings array, unsigned int curStep, unsigned char* strengths, unsigned char* distance_strengths, unsigned char* directional_strengths);
 double getDistance(unsigned char* distance_strengths, int num_strengths);
 int getAngle(unsigned char* directional_strengths, int num_strengths);
+double avg_dbl(double* array, int size);
+double avg_int(int* array, int size);
+int sign(double x);
+double wrapToPi(double theta);
+
+// Control parameters
+double Kp = 1; // Linear speed proportional gain
+double Kw = 4; // Angular speed proportional gain
+double L = 0.21668; // Distance between wheels [m]
+double R = 0.0492125; // Radius of wheels [m]
+double vMax = 0.1; // Maximum linear speed [m/s]
+double omegaMax = M_PI/2; // Maximum angular speed [rad/s]
 
 int main(){
     printf("\tStarting Location Test...\n");
@@ -62,11 +79,11 @@ int main(){
     // Initalize state variables
     int position = 0; // Angular position of reflector array (degrees)
     int direction = 1; // Direction of stepper motor
-    unsigned int current = 0; // Current measurement number
+    unsigned int curStep = 0; // Current step number
     struct XBeeArray_Settings array = {
         5,1,   // Uart buses Top(5) and Side(1) 
-        3,1,  // GPIO 0 (Chip 3 Pin 1)
-        3,2,  // GPIO 1 (Chip 3 Pin 2)
+        3,1,   // GPIO 0 (Chip 3 Pin 1)
+        3,2,   // GPIO 1 (Chip 3 Pin 2)
         0x1111 // Target Remote XBee's 16-bit address
     };
 
@@ -75,8 +92,9 @@ int main(){
     unsigned char strengths[5]; // Temporary storage during measurement
     unsigned char distance_strengths[NUM_MEASUREMENTS]; // Strengths from transmitter
     unsigned char directional_strengths[4*NUM_MEASUREMENTS]; // Strengths from side XBees
-    double distance; // Distance to remote in m
-    int angle; // Angle to remote in degrees
+    double distance[MOVING_AVG_SIZE]; // Distances to remote in m
+    int angle[MOVING_AVG_SIZE]; // Angles to remote in degrees
+    unsigned int curMsmt = 0; // Next location to store the distance and angle in the moving average arrays
 
     // Initalize XBee Reflector Array
     printf("\tInitializing XBee Reflector Array...\n");
@@ -91,15 +109,15 @@ int main(){
     // Keep looping until state changes to EXITING
     rc_set_state(RUNNING);
     // Get the initial measurement
-    result = getMeasurement(array, current, strengths, distance_strengths, directional_strengths);
+    result = getMeasurement(array, curStep, strengths, distance_strengths, directional_strengths);
     MAIN_ASSERT(result == 0, "\tERROR: Failed to get measurement.\n");
     while(rc_get_state() != EXITING)
     {
         // Move the stepper motor
         step(&sm, direction, STEPS_PER_MEASUREMENT);
 
-        // Update current measurement number
-        current += direction;
+        // Update current step number
+        curStep += direction;
 
         // Update position and direction
         position += direction*DEG_PER_MEASUREMENT;
@@ -108,16 +126,43 @@ int main(){
         if ((position >= (90 - DEG_PER_MEASUREMENT)) || (position <= 0))
         {
             // Calculate the distance to the remote
-            distance = getDistance(distance_strengths, sizeof(distance_strengths));
-            printf("Distance to remote: %.4f\n", distance);
-            angle = getAngle(directional_strengths, sizeof(directional_strengths));
-            printf("Angle to remote: %d\n", angle);
+            distance[curMsmt] = getDistance(distance_strengths, sizeof(distance_strengths));
+            printf("Distance to remote: %.4f\n", distance[curMsmt]);
+            angle[curMsmt] = getAngle(directional_strengths, sizeof(directional_strengths));
+            printf("Angle to remote: %d\n", angle[curMsmt]);
+            
+            // Update the current measurement pointer
+            curMsmt = (curMsmt + 1) % MOVING_AVG_SIZE;
+
+            // Calculate the coordinates of the remote
+            double avg_dist = avg_dbl(distance, MOVING_AVG_SIZE);
+            double avg_ang = avg_int(angle, MOVING_AVG_SIZE);
+            double targetX = avg_dist*cos(avg_ang);
+            double targetY = avg_dist*sin(avg_ang);
+
+            // Calculate motor control signals
+            double v = sign(targetX)*Kp*avg_dist;
+            v = (abs(v) > vMax ? sign(v)*vMax : v); // Saturate velocity
+            double omega = Kw*wrapToPi(avg_ang);
+            omega = (abs(omega) > omegaMax ? sign(omega)*omegaMax : omega); // Saturate velocity
+
+            double omegaR = (v + omega*L/2)/R; // right wheel angular speed [rad/s]
+            double omegaL = (v - omega*L/2)/R; // left wheel angular speed [rad/s]
+
+            // Convert angular wheel speeds to duty cycles
+            double dutyR = omegaR*0.2/omegaMax;
+            double dutyL = -omegaL*0.2/omegaMax;
+
+            // Set the motor speeds
+            // rc_motor_set(MOTOR_LEFT, dutyL);
+            // rc_motor_set(MOTOR_RIGHT, dutyR);
+
             // Change direction
             direction *= -1;
         }
 
         // Get Strength Values from the XBee Reflector Array
-        result = getMeasurement(array, current, strengths, distance_strengths, directional_strengths);
+        result = getMeasurement(array, curStep, strengths, distance_strengths, directional_strengths);
         MAIN_ASSERT(result == 0, "\tERROR: Failed to get measurement.\n");
     }
 
@@ -136,17 +181,17 @@ int main(){
     return result;
 }
 
-int getMeasurement(struct XBeeArray_Settings array, unsigned int current, unsigned char* strengths, unsigned char* distance_strengths, unsigned char* directional_strengths)
+int getMeasurement(struct XBeeArray_Settings array, unsigned int curStep, unsigned char* strengths, unsigned char* distance_strengths, unsigned char* directional_strengths)
 {
     // Get Strength Values from the XBee Reflector Array
     int result = XBeeArray_GetStrengths(array, strengths);
     ASSERT(result == 0, "\tERROR: Failed to get signal strength values from the XBee Array.\n");
 
     // Store the strengths in the proper locations
-    distance_strengths[current] = strengths[0]; // Transmitter XBee
+    distance_strengths[curStep] = strengths[0]; // Transmitter XBee
     for (int i = 1; i <= 4; ++i) // Side XBees
     {
-        directional_strengths[current + (i - 1)*10] = strengths[i];
+        directional_strengths[curStep + (i - 1)*10] = strengths[i];
     }
 
     return result;
@@ -187,4 +232,42 @@ int getAngle(unsigned char* directional_strengths, int num_strengths)
     // Convert the index to an angle in degrees
     int angle = min_idx*9;
     return angle;
+}
+
+double avg_dbl(double* array, int size)
+{
+    double sum = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        sum += array[i];
+    }
+    return sum/size;
+}
+
+double avg_int(int* array, int size)
+{
+    int sum = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        sum += array[i];
+    }
+    return (double)sum/size;
+}
+
+int sign(double x)
+{
+    return (x > 0) - (x < 0);
+}
+
+double wrapToPi(double theta)
+{
+    while(theta > M_PI)
+    {
+        theta -= M_PI;
+    }
+    while(theta < -M_PI)
+    {
+        theta += M_PI;
+    }
+    return theta;
 }
