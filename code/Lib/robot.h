2929ff481e829c8bf055d683041d3adab4dc189c
Jason Braker <jbraker@mail.bradley.edu>
2021-04-08 15:07:27 -0500

    Updated robot.h

--- code/Lib/robot.h
+++ code/Lib/robot.h
@@ -108,8 +108,8 @@ int robot_setSpeeds(Robot_t* robot, double v, double omega)
     // Velocities below this threshold will be treated as 0
     static const double threshold = 1E-3;
 
-    double vR = (v - omega*robot->L/2); // right wheel linear speed [rad/s]
-    double vL = (v + omega*robot->L/2); // left wheel linear speed [rad/s]
+    double vR = (v - omega*robot->L/2); // right wheel linear speed [m/s]
+    double vL = (v + omega*robot->L/2); // left wheel linear speed [m/s]
     
     double dutyR = 0;
     double dutyL = 0;
@@ -118,14 +118,14 @@ int robot_setSpeeds(Robot_t* robot, double v, double omega)
     if (fabs(vR) > threshold)
     {
         // Convert linear wheel speed to duty cycle by dividing by the maximum speed
-        dutyR = sign(vR)*clamp(vR/vRMax, minDuty, 1.0); // Right wheel duty cycle
+        dutyR = sign(vR)*clamp(fabs(vR)/vRMax, minDuty, 1.0); // Right wheel duty cycle
     }
 
     // Only compute duty cycle if the velocity is above the threshold. Otherwise, leave it at 0
     if (fabs(vL) > threshold)
     {
         // Convert angular wheel speeds to duty cycles by dividing by the maximum speed of each wheel
-        dutyL = sign(vL)*clamp(vL/vLMax, minDuty, 1.0); // Left wheel duty cycle
+        dutyL = -sign(vL)*clamp(fabs(vL)/vLMax, minDuty, 1.0); // Left wheel duty cycle
     }
 
 #if !MOTORS_OFF
