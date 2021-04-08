9a7bfa259df45a17bfff5f5d5c881b1ae2b80d31
CodeingerCat <37391829+CodeingerCat@users.noreply.github.com>
2021-04-08 13:33:06 -0500

    core_printf

--- code/Lib/core.h
+++ code/Lib/core.h
@@ -37,7 +37,8 @@ typedef unsigned char ubyte;
 
 /// ----- Prototypes ----- ///
 
-void msleep(int milliSeconds);
+inline void msleep(int milliSeconds);
+inline void core_printf( const char* format, ...);
 void printHexBuffer(void* buf, int bufSize);
 void fprintHexBuffer(void* buf, int bufSize, char* header, char* footer);
 //void core_Print();
@@ -46,7 +47,17 @@ void fprintHexBuffer(void* buf, int bufSize, char* header, char* footer);
 
 // Milliseconds Sleep
 // @param milliSeconds: Time in milliseconds to sleep for
-void msleep(int milliSeconds) { usleep((milliSeconds)*1000); }
+inline void msleep(int milliSeconds) { usleep((milliSeconds)*1000); }
+
+// Core Formated Print
+inline void core_printf( const char* format, ...) {
+#ifndef CORE_DISABLE_MESSAGES
+    va_list argptr;
+    va_start(argptr, format);
+    printf(format, argptr); 
+    va_end(argptr);
+#endif
+}
 
 // Print out byte array in hexidecimal
 // @param buf: Pointer to array of bytes to print out
